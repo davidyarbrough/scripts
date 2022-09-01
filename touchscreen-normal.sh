3caf9682755400ba500079f3fe8867b30e622fe6
David Yarbrough <yarbroughdavidc@gmail.com>
2022-08-31 23:19:48 -0500

    parameterizing the touchscreen name

--- touchscreen-normal.sh
+++ touchscreen-normal.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# to find TOUCHSCREEN_NAME, run `xinput -list` and add the touchscreen
+# name as an env var to .zshrc
+
 xrandr -o normal
-xinput set-prop "Elan Touchscreen" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
+xinput set-prop "$TOUCHSCREEN_NAME" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
